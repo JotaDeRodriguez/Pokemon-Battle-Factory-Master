01f553a2f91da59d95caa8b2bea0cd7523cab4e2
Juan Rodriguez <153087028+JotaDeRodriguez@users.noreply.github.com>
2024-06-08 15:04:33 +0200

    Create get_abilities.py

---
+++ get_abilities.py
@@ -0,0 +1,23 @@
+from json_Reader import load_type_data
+
+ability_info = load_type_data(r"C:\Users\juandavid.rodriguez\Documents\Langchain\PokemonProject\Gen III - Abilities.json")
+
+
+def get_pokemon_ability(pokemon_name):
+
+    ability_list = []
+
+    for ability in ability_info:
+        description = ability_info[ability]['Description']
+        learnt_by = ability_info[ability]['Learnt By']
+
+        if pokemon_name in learnt_by:
+            full_description = str(ability + ":" + " " + description)
+            ability_list.append(full_description)
+
+    if len(ability_list)>1:
+        string = "The pokemon can have one of two possible abilities:"
+        string_list = str(string + " " + str(ability_list))
+        return string_list
+    else:
+        return ability_list
